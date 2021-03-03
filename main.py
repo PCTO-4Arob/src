eacb0093d0bc3d0ddd2aa8e9faa7321968b36e70
filippoferrando <filippo.ferrando@itiscuneo.eu>
2021-03-03 17:39:13 +0100

    update

--- main.py
+++ main.py
@@ -2,32 +2,24 @@ from threading import Thread
 import threading
 
 import time
-from Libraries.reconAudio import reconAudio
-#from startMenu import generateOperations
 
 threadLock = threading.Lock() #sync thread 
 
-global text #result of speech-to-text func
-global list
-global startSpeechToText
+global handX, handY
 
-class SpeechToText(Thread):
+class HandRecon(Thread):
     def __init__(self, nome):
         Thread.__init__(self)
         self.nome = nome
         #self.durata = 5
 
     def run(self):
-        global text
+        global handX, handY
 
         print("start capture...")
 
         threadLock.acquire()
-        text = reconAudio()
-
-        if text == False:
-            print("Errors has occurred in reconnaissance...")
-
+        
         threadLock.release()
 
 class GraphicInterface(Thread):
