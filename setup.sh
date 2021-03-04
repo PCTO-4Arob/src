f3952f3d4b6b993ab528ae1f0215e5d6d4d0c948
filippoferrando <filippo.ferrando@itiscuneo.eu>
2021-03-04 12:16:28 +0100

    setup test

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@
 echo Ubuntu script:
 
 echo Refresh repos...
-$(sudo apt update > log.txt)
+$(sudo apt update > log.txt 2>&1)
 
 echo Installing Python...
-$(sudo apt install python3 >> log.txt)
+$(sudo apt install python3 >> log.txt 2>&1)
 
 echo Installing pip...
-$(sudo apt install python3-pip >> log.txt)
+$(sudo apt install python3-pip >> log.txt 2>&1)
 
 echo Installing necessary packages:
 $(sudo pip install -r requirements.txt)
