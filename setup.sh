9229a2dfd1db251d7c418123f785138c3df05a8d
filippoferrando <filippo.ferrando@itiscuneo.eu>
2021-03-04 12:10:09 +0100

    setup test

--- setup.sh
+++ setup.sh
@@ -1,60 +1,16 @@
 #!/bin/bash
 
-echo Detecting OS...
+echo Ubuntu script:
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  local DISTRIB=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
-  if [[ ${DISTRIB} = "Ubuntu"* ]]; then
-    if uname -a | grep -q '^Linux.*Microsoft'; then
-      # ubuntu via WSL Windows Subsystem for Linux
-    else
-      echo Refresh Repos...
-      $(sudo apt update)
-      echo Installing Python...
-      $(sudo apt install python3)
-      echo Installing pip...
-      $(sudo apt install python3-pip)
+echo Refresh repos...
+$(sudo apt update)
 
-      echo Installing necessary packages...
-      $(sudo pip install opencv-python pygame SpeechRecognition numpy)
-    fi
-  elif [[ ${DISTRIB} = "Debian"* ]]; then
-    echo Refresh Repos...
-    $(sudo apt update)
-    echo Installing Python...
-    $(sudo apt install python3)
-    echo Installing pip...
-    $(sudo apt install python3-pip)
+echo Installing Python...
+$(sudo apt install python3)
 
-    echo Installing necessary packages...
-    $(sudo pip install opencv-python pygame SpeechRecognition numpy)
-  fi
+echo Installing pip...
+$(sudo apt install python3-pip)
 
-  elif [[ ${DISTRIB} = "Pop"* ]]; then
-    echo Refresh Repos...
-    $(sudo apt update)
-    echo Installing Python...
-    $(sudo apt install python3)
-    echo Installing pip...
-    $(sudo apt install python3-pip)
+echo Installing necessary packages:
+$(sudo pip install requirements.txt)
 
-    echo Installing necessary packages...
-    $(sudo pip install opencv-python pygame SpeechRecognition numpy)
-  fi
-
-  elif [[ ${DISTRIB} = "Manjaro"*]]; then
-     echo Refresh Repos...
-     $(sudo pacman -Syuu)
-     echo Installing Python...
-     $(sudo pacman -S python3)
-
-     echo Installing pip...
-     $(sudo pacman -S python3-pip)
-
-     echo Installing necessary packages... 
-     $(sudo pip install opencv-python pygame SpeechRecognition numpy)
-   fi
-
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  # macOS OSX
-fi
