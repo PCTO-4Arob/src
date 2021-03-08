6aba8e9b5f8a89c4b770ecca82b418ede02694b9
filippoferrando <filippo.ferrando@itiscuneo.eu>
2021-03-08 10:26:15 +0100

    adjusted comment voice recon, removed pycache

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import time
 import sys
 
-from Libraries.startMenu.startMenu import menu
-from GraphicMain.gameDesign import graficaPricipale
+from Libraries.startMenu.startMenu import menu #import primary library for menu creation
+from GraphicMain.gameDesign import graficaPricipale #import primaru library form graphic
 
 def main():
 
     exitMenu = -1
     exitGame = -1
 
-
     while exitMenu != 0:
        
         exitMenu = menu()
+        
         if exitMenu == 0:
             exit(0)
         elif exitMenu == 1:
