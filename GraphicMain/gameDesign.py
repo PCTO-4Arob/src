ce5338f9de5996fd7a7c9d9df451d33fe6dedb88
filippoferrando <filippo.ferrando@itiscuneo.eu>
2021-03-08 12:16:42 +0100

    fixed sound

--- GraphicMain/gameDesign.py
+++ GraphicMain/gameDesign.py
@@ -149,8 +149,8 @@ def mainGraphic(screen):
     
     #song
     soundTrack = pygame.mixer.Sound(os.path.join(pathname, "theme/fight.wav"))
-    pygame.mixer.Sound.set_volume(a,0.4)#0.6 is volume
-    pygame.mixer.Sound.play(a,-1)#with -1 sound will restart forever
+    pygame.mixer.Sound.set_volume(soundTrack,0.4)#0.6 is volume
+    pygame.mixer.Sound.play(soundTrack,-1)#with -1 sound will restart forever
 
     #CREATION SPRITES-------------------------------------------------------------------------------------------------------
     all_sprites = pygame.sprite.Group()
