7e1f965000796d9b8c32643b3a336de7064bc48b
filippoferrando <filippo.ferrando@itiscuneo.eu>
2021-03-08 10:29:52 +0100

    riadded main

---
+++ main.py
@@ -0,0 +1,35 @@
+import time
+import sys
+
+from Libraries.startMenu.startMenu import menu #import primary library for menu creation
+from GraphicMain.gameDesign import graficaPricipale #import primaru library form graphic
+
+def main():
+    
+    exitMenu = -1
+    exitGame = -1
+
+<<<<<<< HEAD
+=======
+    '''
+>>>>>>> 9b257fe73bd41016f16ecd136df08a572adf911a
+    while exitMenu != 0:
+       
+        exitMenu = menu()
+        
+        if exitMenu == 0:
+            exit(0)
+        elif exitMenu == 1:
+            while exitGame != 0:
+                exitGame = graficaPricipale()
+    '''
+    
+    graficaPricipale()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
